6bc255ac48fa1d96fc1afc84e62f19843add2f93
yaaaniks <yansemikozov03@icloud.com>
2022-11-28 02:38:35 +0300

    Adding exception in 'letter_detection' and fixed 'letter_searching'

--- letter_detection.py
+++ letter_detection.py
@@ -2,6 +2,7 @@ import time
 import easyocr
 import os
 
+
 alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р',
             'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 
@@ -24,7 +25,7 @@ def letter_detection(image, let_arr):
     else:
         for char in result:
             if any(checking_char.isdigit() for checking_char in char):
-                print('number is detected')
+                print('На изображении найдена буква!')
                 time.sleep(1)
                 return False
         if check_for_russian(result):
@@ -36,23 +37,36 @@ def letter_detection(image, let_arr):
 
 
 if __name__ == "__main__":
-    file_shift = open("shift.txt", "r+")
-    shift = int(file_shift.read())
-    file_shift.close()
-    dirname = 'Photos'
-    word = []
-    if os.path.isdir(dirname):
-        files = os.listdir(dirname)
-        print("получены файлы: ", files)
-        for file in files:
-            with open(os.path.join(dirname, file), 'rb+') as f:
-                image = f.read()
-                if letter_detection(image, word):
-                    print("сейчас список состоит из ", word)
-                else:
-                    print("на изображении ничего не найдено")
-        for i in range(len(word)):
-            word[i] = alphabet[i + shift]
-        print(word)
-    else:
-        print('такой папки не существует')
+    try:
+        file_shift = open("shift.txt", "r+")
+        shift = int(file_shift.read())
+        file_shift.close()
+        dirname = 'Photos'
+        word = []
+        new_word = ''
+        if os.path.isdir(dirname):
+            files = os.listdir(dirname)
+            print("Получены файлы: ", files)
+            for file in files:
+                with open(os.path.join(dirname, file), 'rb+') as f:
+                    image = f.read()
+                    if letter_detection(image, word):
+                        print("Сейчас список состоит из ", word)
+                    else:
+                        print("На изображении ничего не найдено")
+            for i in range(len(word)):
+                for j in range(len(alphabet)):
+                    if alphabet[j] == word[i]:
+                        print(alphabet[j])
+                        if (j + shift) > len(alphabet):
+                            new_word += alphabet[j + shift - len(alphabet)]
+                        elif (j + shift) < 0:
+                            new_word += alphabet[len(alphabet) + j + shift]
+                        else:
+                            new_word += alphabet[j + shift]
+            print(new_word)
+        else:
+            print('Такой папки не существует')
+    except FileNotFoundError:
+        print('Ключа к шифру нет!')
+
