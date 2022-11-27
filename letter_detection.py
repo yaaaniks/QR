be62481a6a63c090c17c3f3627120cc3860c8fbd
yaaaniks <yansemikozov03@icloud.com>
2022-11-28 02:54:21 +0300

    Minor changes

--- letter_detection.py
+++ letter_detection.py
@@ -2,20 +2,11 @@ import time
 import easyocr
 import os
 
-
 alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р',
             'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 
 
-def check_for_russian(string):
-    for letter in string:
-        if any(checking_letter in alphabet for checking_letter in letter):
-            return letter
-    else:
-        return False
-
-
-def letter_detection(image, let_arr):
+def letter_writing(image, let_arr):
     gpu = False
     reader = easyocr.Reader(['ru'], gpu=gpu)
     result = reader.readtext(image, detail=0)
@@ -25,15 +16,29 @@ def letter_detection(image, let_arr):
     else:
         for char in result:
             if any(checking_char.isdigit() for checking_char in char):
-                print('На изображении найдена буква!')
+                print('На изображении найдена цифра!')
                 time.sleep(1)
                 return False
-        if check_for_russian(result):
-            let_arr.append(check_for_russian(result))
-            return True
-        else:
-            time.sleep(1)
-            return False
+            elif any(checking_letter in alphabet for checking_letter in char):
+                let_arr.append(char)
+                print("К списку присоединена  ", char)
+                return True
+            else:
+                time.sleep(1)
+                return False
+
+
+def decoding(new_word, word):
+    for i in range(len(word)):
+        for j in range(len(alphabet)):
+            if alphabet[j] == word[i]:
+                print(alphabet[j])
+                if (j + shift) > len(alphabet):
+                    new_word += alphabet[j + shift - len(alphabet)]
+                elif (j + shift) < 0:
+                    new_word += alphabet[len(alphabet) + j + shift]
+                else:
+                    new_word += alphabet[j + shift]
 
 
 if __name__ == "__main__":
@@ -50,23 +55,13 @@ if __name__ == "__main__":
             for file in files:
                 with open(os.path.join(dirname, file), 'rb+') as f:
                     image = f.read()
-                    if letter_detection(image, word):
+                    if letter_writing(image, word):
                         print("Сейчас список состоит из ", word)
                     else:
                         print("На изображении ничего не найдено")
-            for i in range(len(word)):
-                for j in range(len(alphabet)):
-                    if alphabet[j] == word[i]:
-                        print(alphabet[j])
-                        if (j + shift) > len(alphabet):
-                            new_word += alphabet[j + shift - len(alphabet)]
-                        elif (j + shift) < 0:
-                            new_word += alphabet[len(alphabet) + j + shift]
-                        else:
-                            new_word += alphabet[j + shift]
+            decoding(new_word, word)
             print(new_word)
         else:
             print('Такой папки не существует')
     except FileNotFoundError:
         print('Ключа к шифру нет!')
-
